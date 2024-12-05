198cb84b6cf1db86035208e42669fc1749a594ac
TheJohny71 <jduval71@me.com>
2024-12-05 16:36:05 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
 import React from 'react';
+import WelcomePage from './components/WelcomePage';
 
 function App() {
-  return (
-    <div className="min-h-screen bg-gray-50">
-      <h1 className="text-3xl text-center pt-20 font-bold">
-        Alfie
-      </h1>
-    </div>
-  );
+  return <WelcomePage />;
 }
 
 export default App;
