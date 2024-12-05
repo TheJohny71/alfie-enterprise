1fb9378a08c2749d0dd3b5e51a27f0f9b316ea91
TheJohny71 <jduval71@me.com>
2024-12-05 16:56:36 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/alfie-enterprise/', // This should match your repository name
+  base: '/alfie-enterprise/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-});
+})
