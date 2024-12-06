d5ee5b22576937e123adee7b8fb5b8afa3e8ef5c
TheJohny71 <jduval71@me.com>
2024-12-06 14:13:58 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -10,11 +11,20 @@ export default defineConfig({
     },
   },
   build: {
-    outDir: 'dist',
-    sourcemap: true,
+    rollupOptions: {
+      external: ['recharts'],
+      output: {
+        globals: {
+          recharts: 'Recharts'
+        }
+      }
+    },
+    commonjsOptions: {
+      include: [/node_modules/],
+      transformMixedEsModules: true
+    }
   },
-  server: {
-    port: 3000,
-    open: true,
-  },
-});
+  optimizeDeps: {
+    include: ['recharts']
+  }
+})
