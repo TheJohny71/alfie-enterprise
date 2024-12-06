91c4dac9a7923bb25134be7a10db1513748bc637
TheJohny71 <jduval71@me.com>
2024-12-06 09:49:57 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,16 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/alfie-enterprise/'
+  base: '/alfie/',
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          utils: ['lodash']
+        }
+      }
+    }
+  }
 })
