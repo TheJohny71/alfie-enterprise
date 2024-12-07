e160ce209c3cafd89e2de7615132ede2e50af454
TheJohny71 <jduval71@me.com>
2024-12-06 23:26:39 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-// No need to import React if never directly referenced, thanks to new JSX transform.
-// If you have React 17+ and are using react-jsx runtime, omit `import React from 'react';`
-
 import WelcomePage from '@/components/ui/WelcomePage';
 
 function App() {
