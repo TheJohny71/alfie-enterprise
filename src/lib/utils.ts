7dbba4743ba76f60bf19979b26274c0906f9864a
TheJohny71 <jduval71@me.com>
2024-12-09 11:26:31 -0600

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,12 +1,6 @@
-// src/lib/utils.ts
-export function formatDate(date: Date): string {
-  return new Intl.DateTimeFormat('en-US', {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric'
-  }).format(date);
-}
+import { ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
 
-export function generateId(): string {
-  return Math.random().toString(36).substring(2, 9);
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
 }
