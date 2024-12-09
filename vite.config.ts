1b931ef5abea5d4aa66cac81df3de69cd5770f00
TheJohny71 <jduval71@me.com>
2024-12-09 10:32:31 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
-/vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true
+    },
+    sourcemap: true
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
