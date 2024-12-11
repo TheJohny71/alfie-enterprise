0a7adf223f9d1cf1d56ddfeb210edea40fb88b49
TheJohny71 <jduval71@me.com>
2024-12-11 10:44:38 -0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
-        background: '#F5F5F7', // Used for the bg-background class
+        background: '#F5F5F7',
       },
     },
   },
