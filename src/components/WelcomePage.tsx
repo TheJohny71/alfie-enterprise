632a01f1ad6b8fe9007a25e64d212b5e9127a9b7
TheJohny71 <jduval71@me.com>
2024-12-11 13:03:07 -0600

    Update WelcomePage.tsx

--- src/components/WelcomePage.tsx
+++ src/components/WelcomePage.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 
-// Add any props or functionality your WelcomePage may need
 const WelcomePage = () => {
   return (
     <motion.div
