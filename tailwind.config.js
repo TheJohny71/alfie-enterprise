44a378ebcd1399b44175761d6b1c35818c4cea47
TheJohny71 <jduval71@me.com>
2024-12-10 22:01:18 -0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,16 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#5E5CE6",
+        background: "#F5F5F7"
+      }
+    }
   },
+  plugins: []
 }
